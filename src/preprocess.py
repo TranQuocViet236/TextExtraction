b9c6aa3f50267d0c93a862f362188c32962ab614
TranQuocViet236 <vietviettrantran004@gmail.com>
2022-09-05 23:02:45 +0700

    Initial commit

---
+++ src/preprocess.py
@@ -0,0 +1,34 @@
+class SquadExample:
+    def __init__(self, question, context, start_char_idx, answer_text, all_answers):
+        self.question = question
+        self.context = context
+        self.start_char_idx = start_char_idx
+        self.answer_text = answer_text
+        self.all_answers = all_answers
+        self.skip = False
+
+    def preprocess(self):
+        context = self.context
+        question = self.question
+        answer_text = self.answer_text
+        start_char_idx = self.start_char_idx
+
+        # clean context, answer, and question
+        context = " ".join(str(context).split())
+        question = " ".join(str(question).split())
+        answer = " ".join(str(answer_text).split())
+
+        # Find end character index of answer in context
+        end_char_idx = start_char_idx + len(answer)
+        if end_char_idx >= len(context):
+            self.skip = True
+            return
+
+        # Mark the character indexes in context that are in answer
+        is_char_in_ans = [0]*len(context)
+
+        for idx in range(start_char_idx, end_char_idx):
+            is_char_in_ans[idx] = 1
+
+        # Tokenize contexx
+        tokenized_context = tokenizer.encode(question)
